b147882c7270dd3fefdc992241de58afebbf917c
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2024-03-20 23:29:47 +0600

    update

--- JS/Lec6.js
+++ JS/Lec6.js
@@ -1,4 +1,25 @@
 console.log("Lecture 6 Starts From Here"); 
 console.log("DOM Starts From Here"); 
 console.log("DOM Starts TEst Here"); 
-alert("Hello World");
+//alert("Hello World");
+document.write("Hello World");
+
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
+
+let paragraphs = document.getElementsByTagName("p");
+console.dir(paragraphs);
+console.log(paragraphs);
+
+let elements = document.querySelectorAll(".heading-class"); //1st element
+console.dir(elements);
+
+let firstEl = document.querySelector("p"); //1st element
+console.dir(firstEl);
+
+
+
